37accda17254fb779642f69376da576ca2eec92e
Ruban Jeno <Ruban Jeno@Ruban-jeno>
2024-04-24 17:09:03 +0530

    Hi bro

---
+++ Dominos/src/main/java/com/Pom/Pom_Dominos.java
@@ -0,0 +1,11 @@
+package com.Pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Pom_Dominos {
+@FindBy(xpath="//a[.=\"OUR MENU\"]")
+private WebElement menu;
+
+
+}
